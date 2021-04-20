3494901de7c2a938f38e58e72f7db118ca24e67c
MinjaCheng <minja.cheng@iths.se>
2021-04-20 14:07:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p id="welcome-text">Welcome to</p>
+        <h1>DOGGY DAYCARE</h1>
+        <Welcome />
       </header>
     </div>
   );
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import './welcome.css';
+
+const Welcome = () => (
+    <section>
+       <div className="logo"><img src="img/dog.png" alt="Dog and a kennel"/></div>
+
+        <button className='register-button'>GO TO REGISTER</button>
+    </section>
+
+
+);
+
+export default Welcome;
