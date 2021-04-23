1b891bcc8fba8bc1de7962e379ffd0539a6c5c76
MinjaCheng <minja.cheng@iths.se>
2021-04-23 12:13:30 +0200

    Able to upload data to webpage

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Welcome from './components/Welcome';
 import Register from './components/Register';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p id="welcome-text">Welcome to</p>
-        <h1>DOGGY DAYCARE</h1>
-      </header>
-      <main className='App-main'>
-        <Register />
-      </main>
-    </div>
-  );
+    return (
+        <div className="App" >
+        <header className="App-header" >
+            <p id="welcome-text" > Welcome to 
+            </p> 
+            <h1 > DOGGY DAYCARE </h1>  
+            </header > 
+            <main className='App-main' >
+                <Register />
+            </main> 
+        </div >
+    );
 }
 
 export default App;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,103 +4,43 @@ import React, { useState, useEffect } from 'react';
 
 const Register = () => {
 
-    const url = 'https://api.jsonbin.io/b/607eb43024143e5df089b745';
+    const [fetchData, setFetchData] = useState([]);
 
     useEffect(async () => {
-        try {
-            const response = await fetch(url);
-            console.log('2. Got response', response);
+        
 
-            const data = await response.json();
-            console.log('3. Got data', data);
+            const url = 'https://api.jsonbin.io/b/607eb43024143e5df089b745';
 
-            return data;
-        }
-        catch (err) {
-            console.log(err);
-        }
+            try {
+                const response = await fetch(url);
+                console.log('2. Got response', response);
+
+                const data = await response.json();
+                console.log('3. Got data', data);
+
+                setFetchData(data);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        
     }, []);
+
 
     return (
         <section className='dog-info'>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly</p>
-                <p>Breed: Briard</p>
+            {fetchData.map((d, index) => (
+            <div key={d}>
+                <img src={d.img} alt={d.name} />
+                <p>Name: {d.name}</p>
+                <p>Breed: {d.breed}</p>
                 <p>At Daycare: Yes</p>
                 <nav>More information ></nav>
             </div>
-            <div>
-                <img src="https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_3645.jpg" alt="Molly" />
-                <p>Name: Molly 1</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src="https://images.dog.ceo/breeds/husky/n02110185_8860.jpg" alt="Molly" />
-                <p>Name: Molly 2</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/beagle/n02088364_13214.jpg"} alt="Molly" />
-                <p>Name: Molly 3</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly 4</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly 5</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly 6</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly 7</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly 8</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly 9</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
-            <div>
-                <img src={"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg"} alt="Molly" />
-                <p>Name: Molly 10</p>
-                <p>Breed: Briard</p>
-                <p>At Daycare: Yes</p>
-                <nav>More information ></nav>
-            </div>
+        ))}
         </section>
+
+    
     );
 }
 
