00df11dc414615e2762099cf1d29cc5d05b9d634
MinjaCheng <minja.cheng@iths.se>
2021-05-04 12:51:26 +0200

    Some CSS changes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,10 @@
 import './welcome.css';
 import { Link } from 'react-router-dom';
+import dog from '../dog.png'
 
 const Welcome = () => (
     <section className="welcome-page">
-        <div className="logo"><img src="img/dog.png" alt="Dog and a kennel" /></div>
+        <div className="logo"><img className="logo-img" src={dog} alt="Dog and a kennel" /></div>
         <Link to="/register">
             <button className='register-button'>GO TO REGISTER</button>
         </Link>
